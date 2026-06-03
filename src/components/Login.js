833c58165d2399467edc811c0c7ca79471190550
Kapil Choudhary <kapilchoudhary9171@gmail.com>
2026-06-04 00:09:11 +0530

    fix bug

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,12 @@
+
 // import { useRef, useState } from "react";
 // import Headers from "./Header";
 // import { CheckValidDate } from "../utils/validate";
-// import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+// import { createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword } from "firebase/auth";
 // import { auth } from "../utils/firebase";
-// import { signInWithEmailAndPassword } from "firebase/auth";
 // import { useDispatch } from "react-redux";
-// import {addUser} from "../utils/userSlice";
+// import { addUser } from "../utils/userSlice";
 // import { BG_URL, USER_AVATAR } from "../utils/constants";
-
-
-
 
 // const Login = () => {
 //   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -24,52 +21,50 @@
 //     const message = CheckValidDate(email.current.value, password.current.value);
 //     setErrorMessage(message);
 //     if (message) return;
+
 //     if (!isSignInForm) {
+//       // SIGN UP
 //       createUserWithEmailAndPassword(
 //         auth,
 //         email.current.value,
 //         password.current.value
 //       )
 //         .then((userCredential) => {
-//           const user = userCredential.user;
-//           updateProfile(user, {
+//           updateProfile(userCredential.user, {
 //             displayName: name.current.value,
 //             photoURL: USER_AVATAR,
 //           })
 //             .then(() => {
-//               const {uid , email , displayName , photoURL} = auth.currentUser;
+//               const { uid, email, displayName, photoURL } = auth.currentUser;
 //               dispatch(
 //                 addUser({
-//             uid: uid,
-//             email: email,
-//             displayName: displayName,
-//             photoURL: photoURL,
-//           })
-//       );
+//                   uid,
+//                   email,
+//                   displayName,
+//                   photoURL,
+//                 })
+//               );
 //             })
 //             .catch((error) => {
 //               setErrorMessage(error.message);
 //             });
 //         })
 //         .catch((error) => {
-//           const errorCode = error.code;
-//           const errorMessage = error.message;
-//           setErrorMessage(errorCode + "-" + errorMessage);
+//           setErrorMessage(error.code + "-" + error.message);
 //         });
+
 //     } else {
+//       // SIGN IN
 //       signInWithEmailAndPassword(
 //         auth,
 //         email.current.value,
 //         password.current.value
 //       )
-//         .then((userCredential) => {
-//           const user = userCredential.user;
- 
+//         .then(() => {
+//           // no unused variable now ✅
 //         })
 //         .catch((error) => {
-//           const errorCode = error.code;
-//           const errorMessage = error.message;
-//           setErrorMessage(errorCode + "-" + errorMessage);
+//           setErrorMessage(error.code + "-" + error.message);
 //         });
 //     }
 //   };
@@ -81,26 +76,29 @@
 //   return (
 //     <div>
 //       <Headers />
+
 //       <div className="absolute inset-0 -z-10">
-//   <img 
-//     src={BG_URL}
-//     className="w-full h-full object-cover brightness-50"
-//   />
-// </div>
+//         <img
+//           src={BG_URL}
+//           alt="background"
+//           className="w-full h-full object-cover brightness-50"
+//         />
+//       </div>
 
 //       <form
 //         onSubmit={(e) => e.preventDefault()}
-//         className=" w-[60%] md:w-3/12 absolute p-8 h-15 bg-black  my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90"
+//         className="w-[60%] md:w-3/12 absolute p-8 h-15 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90"
 //       >
 //         <h1 className="text-2xl font-bold py-4">
 //           {isSignInForm ? "Sign In" : "Sign Up"}
 //         </h1>
+
 //         {!isSignInForm && (
 //           <input
-//           ref={name}
+//             ref={name}
 //             type="text"
 //             placeholder="Full Name"
-//             className="my-4 p-4 w-full rounded-md bg-gray-800  bg-opacity-100"
+//             className="my-4 p-4 w-full rounded-md bg-gray-800 bg-opacity-100"
 //           />
 //         )}
 
@@ -108,23 +106,28 @@
 //           ref={email}
 //           type="text"
 //           placeholder="Email Address"
-//           className="my-4 p-4 w-full rounded-md bg-gray-800   bg-opacity-100"
+//           className="my-4 p-4 w-full rounded-md bg-gray-800 bg-opacity-100"
 //         />
 
 //         <input
 //           ref={password}
 //           type="password"
 //           placeholder="Password"
-//           className="my-4 p-4 w-full rounded-md bg-gray-800   bg-opacity-100"
+//           className="my-4 p-4 w-full rounded-md bg-gray-800 bg-opacity-100"
 //         />
-//         <p className="text-red-600  font-bold text-lg py-4">{errorMessage}</p>
+
+//         <p className="text-red-600 font-bold text-lg py-4">
+//           {errorMessage}
+//         </p>
+
 //         <button
-//           className="bg-red-700 w-full p-3 my-6 rounded-lg "
+//           className="bg-red-700 w-full p-3 my-6 rounded-lg"
 //           onClick={handleButtonClick}
 //         >
 //           {isSignInForm ? "Sign In" : "Sign Up"}
 //         </button>
-//         <p className="py-3 mr-23 cursor-pointer " onClick={toggleSingInForm}>
+
+//         <p className="py-3 mr-23 cursor-pointer" onClick={toggleSingInForm}>
 //           {isSignInForm
 //             ? "New to Netflix? Sign Up Now"
 //             : "Already register? Sign In Now"}
@@ -133,8 +136,8 @@
 //     </div>
 //   );
 // };
-// export default Login;
 
+// export default Login;
 
 
 import { useRef, useState } from "react";
@@ -225,7 +228,7 @@ const Login = () => {
 
       <form
         onSubmit={(e) => e.preventDefault()}
-        className="w-[60%] md:w-3/12 absolute p-8 h-15 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90"
+        className="w-[90%] sm:w-[60%] md:w-5/12 lg:w-3/12 absolute p-6 sm:p-8 bg-black my-20 sm:my-28 md:my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-90"
       >
         <h1 className="text-2xl font-bold py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
