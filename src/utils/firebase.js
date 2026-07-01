feea6ca5fca5a05b99033a43aa82bcdcac5f1675
Kapil Choudhary <kapilchoudhary9171@gmail.com>
2026-07-01 23:27:36 +0530

    Add Firebase config fallback

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey:
+    process.env.REACT_APP_FIREBASE_API_KEY ||
+    "AIzaSyBukQwrwNREvDRg_HqjpSlD_nmPROL-xjw",
+  authDomain:
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+    "netflix-gpt-fec36.firebaseapp.com",
+  projectId:
+    process.env.REACT_APP_FIREBASE_PROJECT_ID || "netflix-gpt-fec36",
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
+    "netflix-gpt-fec36.firebasestorage.app",
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "70035192863",
+  appId:
+    process.env.REACT_APP_FIREBASE_APP_ID ||
+    "1:70035192863:web:9ad94f197cb5b79069e92a",
+  measurementId:
+    process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-6KMGPHQ0BY",
 };
 
 const app = initializeApp(firebaseConfig);
